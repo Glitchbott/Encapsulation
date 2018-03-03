a3ea0b1fbbea92aa1a7af238e3643d191bc1342e
Glitchbott <36093622+Glitchbott@users.noreply.github.com>
2018-03-03 19:52:51 +0600

    Object initialization

---
+++ Objectlnitializers/Objectlnitializers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objectlnitializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            Point anotherPoint = new Point();
+            anotherPoint.DisplayStats();
+
+            Point finalPoint = new Point();
+            finalPoint.DisplayStats();
+            Console.ReadLine();
+        }
+    }
+}
